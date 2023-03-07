bc37964401e65203f7e37d690b905fd7e174daaf
herok <heroking2081@gmail.com>
2023-03-07 09:27:53 +0500

    Initial commit

---
+++ src/main/java/job4j/cinema/model/Film.java
@@ -0,0 +1,88 @@
+package job4j.cinema.model;
+
+import java.util.Objects;
+
+public class Film {
+
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private int year;
+
+    private int minimalAge;
+
+    private int durationInMinutes;
+
+    public Film(int id, String name, String description, int year, int minimalAge, int durationInMinutes) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.year = year;
+        this.minimalAge = minimalAge;
+        this.durationInMinutes =durationInMinutes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMinimalAge() {
+        return minimalAge;
+    }
+
+    public void setMinimalAge(int minimalAge) {
+        this.minimalAge = minimalAge;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return id == film.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
